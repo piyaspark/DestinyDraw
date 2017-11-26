3414d731d048acf3965d85968d0615f2e65ed7ec
thanakritfluk <thanakrit.da@ku.th>
2017-11-27 00:17:43 +0700

    Working on process

---
+++ src/destinydraw/Money.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package destinydraw;
+
+/**
+ *
+ * @author spark
+ */
+public class Money {
+
+    public static boolean isEnough(long money) {
+        if (money < 0) {
+            return false;
+        }
+        return true;
+    }
+}
